a58c66aa8aa66ad5151df79ce2d86083b57ca426
Joseph david <jodavid444dave@gmail.com>
2023-02-24 13:50:56 +0000

    Create components and pages folder, and edit sub folders amicably

--- book-app/my-app/src/App.js
+++ book-app/my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Navigation from "./component/Navigation";
+import Categories from "./pages/Categories"
+import Home from "./pages/Home"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+  <Navigation />
+  <Routes>
+    <Route path="/" element={<Home />} />
+      <Route path="categories" element={<Categories />} />
+  </Routes>
+  
+  
+  </>
+);
+
+
 
 export default App;
